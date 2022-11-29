887c6233dd51be1da7ddf1041ec29826131e7bce
hvega25 <hvega@jaumebalmes.net>
2022-11-29 19:58:54 +0100

    Creacion del codigo para insertar

---
+++ insert.py
@@ -0,0 +1,29 @@
+import psycopg2 #importa la libreria necesaria para la base de datos
+
+
+def insert ():
+    try:
+        # la variable coneccion se le asigna el metodo con los parametros
+        connection = psycopg2.connect(
+            host='localhost',
+            user='postgres',
+            password=' ',  # tenemos diferentes contraseñas en el pgadmin4
+            database='vehicles')
+        print("Conexión exitosa")  # imprime si hay conexion es una bandera verde
+        cursor = connection.cursor()  # esta variable es la de control sobre la tabla
+
+        #query necesaria para la ejecucion de la base de datos
+        postgres_insert_query = """ INSERT INTO vehicle (id, marca, precio,color) VALUES (%s,%s,%s,%s)"""
+        record_to_insert = (9, 'Lambo', 60, 'amarillo') #valores
+        cursor.execute(postgres_insert_query, record_to_insert)#ejecucion de la query
+
+        connection.commit() # subida
+        count = cursor.rowcount # conteo de elementos subidos a la tabla
+        print(count, "Insertado correctamente") #
+    except Exception as ex:
+        print(ex)
+    connection.close()  # cierra la conexion
+    print("Cerrada correctamente")
+
+
+insert()
